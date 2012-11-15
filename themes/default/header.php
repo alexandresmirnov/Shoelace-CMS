8dad37f0b4c39c91dc04c0d572080a42a5800c47
Sasha Smirnov <designlunatic@gmail.com>
2012-11-15 09:37:51 -0800

    Made the <title> tag display the site name

--- themes/default/header.php
+++ themes/default/header.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-	<title>Shoelace CMS</title>
+	<title><?php echo $settings->siteName ?></title>
 	<meta name="description" content="">
 	<meta name="author" content="">
 
