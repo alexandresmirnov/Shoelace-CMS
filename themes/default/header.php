ac0fd9ddce080ab545d468a9dadf9634cce6382c
Sasha Smirnov <designlunatic@gmail.com>
2012-11-14 21:02:02 -0800

    Updated the title tag

--- themes/default/header.php
+++ themes/default/header.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-	<title>Alexandre Smirnov</title>
+	<title>Shoelace CMS</title>
 	<meta name="description" content="">
 	<meta name="author" content="">
 
